eb410fae51a373285f91e7c6126b9dfc781ae721
FORGIS98 <jorgesolgonzalez1998@gmail.com>
2021-05-24 13:05:32 +0200

    comentarios añadidos

--- Estublock/EasyBlockchain/src/main/java/com/example/easyblockchain/TransactionsHelper.java
+++ Estublock/EasyBlockchain/src/main/java/com/example/easyblockchain/TransactionsHelper.java
@@ -16,15 +16,38 @@ import org.web3j.utils.Numeric;
 import java.io.IOException;
 import java.math.BigInteger;
 
+/**
+ * TransactionsHelper es una clase que permite firmar y enviar transacciones
+ * a una red blockchain basada en Ethereum. Utiliza la librería web3j,
+ * por lo que cualquier red blockchain que disponga de comunicación con JSON-RPC 
+ * permite el uso de esta clase.
+ *
+ * @author Jorge Sol Gonzalez
+ * {@link https://github.com/FORGIS98/}
+ */
 public class TransactionsHelper {
 
   Web3j web3j;
 
+  /**
+   * Constructor de la clase.
+   * @param blockchainURL La URL y puerto al nodo de la red blockchain con la que se
+   * quiere comunicar. 
+   */
   public TransactionsHelper(@NonNull String blockchainURL){
     web3j = Web3j.build(new HttpService(blockchainURL));
   }
 
-  public void signTransaction(@NonNull Credentials credentials, @NonNull String gasPrice, @NonNull String gas, @NonNull String to, @NonNull String data, @NonNull EasyBlockchainListener listener){
+  /**
+   * Método que firma una transacción.
+   * @param credentials Las credenciales de un usuario, viene a ser su keystore
+   * @param gasPrice Cantidad que quieres pagar por unidad de gas como tarifa al minero
+   * @param gasLimit Limite máximo que estas dispuesto a pagar por la transacción
+   * @param to Address del SMART CONTRACT de destino
+   * @param data Información que se va a mandar al smart contract
+   * @param listener Callback de la llamada a sobreescribir
+   */
+  public void signTransaction(@NonNull Credentials credentials, @NonNull String gasPrice, @NonNull String gasLimit, @NonNull String to, @NonNull String data, @NonNull EasyBlockchainListener listener){
     new Thread(new Runnable() {
       @Override
       public void run() {
@@ -35,7 +58,7 @@ public class TransactionsHelper {
           RawTransaction rawTransaction = RawTransaction.createTransaction(
               nonce,
               Numeric.toBigInt(gasPrice),
-              Numeric.toBigInt(gas),
+              Numeric.toBigInt(gasLimit),
               to,
               data
           );
@@ -48,6 +71,11 @@ public class TransactionsHelper {
     }).start();
   }
 
+  /**
+   * Método que envía una transacción ya firmada con listener.
+   * @param signedMessage Transacción firmada lista para ser enviada
+   * @param listener Callback de la llamada a sobreescribir
+   */
   public void sendSignedTransaction(@NonNull String signedMessage, @NonNull EasyBlockchainListener listener){
     new Thread(new Runnable() {
       @Override
@@ -62,6 +90,10 @@ public class TransactionsHelper {
     }).start();
   }
 
+  /**
+   * Método que envía una transacción ya firmada sin listener.
+   * @param signedMessage Transacción firmada lista para ser enviada
+   */
   public void sendSignedTransaction(@NonNull String signedMessage){
     new Thread(new Runnable() {
       @Override
@@ -75,8 +107,17 @@ public class TransactionsHelper {
     }).start();
   }
 
-  public void signAndSendTransaction(@NonNull Credentials credentials, @NonNull String gasPrice, @NonNull String gas, @NonNull String to, @NonNull String data){
-    this.signTransaction(credentials, gasPrice, gas, to, data, new EasyBlockchainListener() {
+  /**
+   * Método que firma y envía una transacción.
+   * @param credentials Las credenciales de un usuario, viene a ser su keystore
+   * @param gasPrice Cantidad que quieres pagar por unidad de gas como tarifa al minero
+   * @param gasLimit Limite máximo que estas dispuesto a pagar por la transacción
+   * @param to Address del SMART CONTRACT de destino
+   * @param data Información que se va a mandar al smart contract
+   * @param listener Callback de la llamada a sobreescribir
+   */  
+  public void signAndSendTransaction(@NonNull Credentials credentials, @NonNull String gasPrice, @NonNull String gasLimit, @NonNull String to, @NonNull String data){
+    this.signTransaction(credentials, gasPrice, gasLimit, to, data, new EasyBlockchainListener() {
       @Override
       public void onSignTransactionEvent(@NonNull @NotNull String signedTx) {
         sendSignedTransaction(signedTx);
--- Estublock/EasyBlockchain/src/main/java/com/example/easyblockchain/WalletHelper.java
+++ Estublock/EasyBlockchain/src/main/java/com/example/easyblockchain/WalletHelper.java
@@ -19,14 +19,28 @@ import java.security.NoSuchProviderException;
 import java.security.Provider;
 import java.security.Security;
 
+/**
+ * WalletHelper es una clase que permite generar y guardar carteras virtuales
+ * para poder usarlas en redes blockchain. Utiliza la librería web3j
+ * que permite crear carteras virtuales compatibles con redes basadas en Ethereum
+ *
+ * @author Jorge Sol Gonzalez
+ * {@link https://github.com/FORGIS98/}
+ */
 public class WalletHelper {
 
+  /**
+   * Constructor de la clase, llama a la función workaroundECDA()
+   */
   public WalletHelper(){
     workaroundECDA();
   }
 
+  /**
+   * Método que arregla el problema con el proveedor de seguridad
+   * Para saber más ir a este link: {@link https://github.com/web3j/web3j/issues/915}
+   */
   private void workaroundECDA(){
-    // See: https://github.com/web3j/web3j/issues/915
     final Provider provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME);
     if(provider != null || !provider.getClass().equals(BouncyCastleProvider.class)){
       Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME);
@@ -34,19 +48,33 @@ public class WalletHelper {
     }
   }
 
+  /**
+   * Método que crea una nueva cartera virtual
+   * @param password Contraseña de la cartera virtual
+   * @param walletDirectory Dirección de la nueva cartera virtual
+   */
   @NonNull
   public String createNewWallet(@NonNull String password, @NonNull File walletDirectory)
       throws CipherException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, IOException {
     return WalletUtils.generateLightNewWalletFile(password, walletDirectory);
   }
 
-  @NonNull
+  /**
+   * Método que crea una nueva cartera virtual
+   * @param password Contraseña de la cartera virtual
+   * @param walletDirectory Dirección de la nueva cartera virtual
+   */  @NonNull
   public String createNewWallet(@NonNull String password, @NonNull String walletDirectory)
       throws CipherException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, IOException {
     return WalletUtils.generateLightNewWalletFile(password, new File(walletDirectory));
   }
 
 
+  /**
+   * Método que devuelve el address de una cartera
+   * @param password Contraseña de la cartera virtual
+   * @param keyStoreDirectory Dirección de la cartera virtual
+   */  
   @NonNull
   public String getAddress(@NonNull String password, @NonNull File keyStoreDirectory)
       throws IOException, CipherException {
@@ -54,6 +82,12 @@ public class WalletHelper {
     return credentials.getAddress();
   }
 
+  /**
+   * Devuelve el address de una cartera
+   * @param password Contraseña de la cartera virtual
+   * @param keyStoreDirectory Dirección de la cartera virtual
+   * @return address de la cartera
+   */ 
   @NonNull
   public String getAddress(@NonNull String password, @NonNull String keyStoreDirectory)
       throws IOException, CipherException {
@@ -61,13 +95,25 @@ public class WalletHelper {
     return credentials.getAddress();
   }
 
-
+  /**
+   * Devuelve las credenciales
+   * @param password Contraseña de la cartera virtual
+   * @param keyStoreDirectory Dirección de la cartera virtual
+   * @return credenciales de la cartera virtual
+   */ 
   @NonNull
   public Credentials getCredentials(@NonNull String password, @NonNull File keyStoreDirectory)
       throws IOException, CipherException {
     return WalletUtils.loadCredentials(password, keyStoreDirectory);
   }
 
+  /**
+   * Guarda en el sharedPreferences de android un identificador y la dirección del wallet
+   * @param id id para la dirección del wallet
+   * @param walletDirectory Dirección de la cartera virtual
+   * @param prefsName nombre de las shared preferences que se quiere usar
+   * @param activity Actividad de android para poder cargar las shared preferences
+   */ 
   public void saveInPreferences(@NonNull String id, @NonNull String walletDirectory, @NonNull String prefsName, @NonNull Activity activity){
     SharedPreferences sharedPreferences = activity.getSharedPreferences(prefsName, Context.MODE_PRIVATE);
     SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -75,6 +121,13 @@ public class WalletHelper {
     editor.apply();
   }
 
+  /**
+   * Devuelve el valor asociado al id
+   * @param id id del que se quiere su valor
+   * @param prefsName nombre de las shared preferences que se quiere usar
+   * @param activity Actividad de android para poder cargar las shared preferences
+   * @return Devuelve la dirección donde esta guardar la cartera virtual de la persona
+   */ 
   @NonNull
   public String getFromPreferences(@NonNull String id, @NonNull String prefsName, @NonNull Activity activity){
     SharedPreferences sharedPreferences = activity.getSharedPreferences(prefsName, Context.MODE_PRIVATE);
